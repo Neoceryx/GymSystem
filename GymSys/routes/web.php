0fdfbba3f579278288e0dd8c286c64843618644d
Neoceryx <dani-deoz@hotmail.com>
2017-08-27 22:08:23 -0700

    Se agregaron las rutas de prueba

--- GymSys/routes/web.php
+++ GymSys/routes/web.php
@@ -14,3 +14,34 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+{ /* Region Test Routes */
+
+  Route::get("Test",function ($value='') {
+    return "Hello World";
+  });
+
+  Route::get("Home",function ($value='') {
+
+    // Loads the Home view
+    return view("Home");
+
+  });
+
+  // route whit parameters. ? validate if the variable has value
+  Route::get("Param/{Name?}",function ($Name='Daniel') {
+
+    // Display $Name values
+    return "Hi ". $Name;
+
+  });
+
+  // Multiples Parameters on a Url.
+  Route::get("MulParam/{Name?}/{Age?}",function ($Name='Daniel', $Age="24") {
+
+    // Display $Name and $Age Val
+    return "The name is: ".$Name. " And the Age is ".$Age;
+
+  });
+
+} /* End Region */
