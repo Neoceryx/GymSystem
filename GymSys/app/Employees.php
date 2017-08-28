c39d350dc48b1a38b52dbb6c3e19ae7b294c1adc
Neoceryx <dani-deoz@hotmail.com>
2017-08-27 23:36:19 -0700

    Se creo el metodo en el controlador Employees para obtener la lista de todos los empleados

---
+++ GymSys/app/Employees.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace GymSys;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employees extends Model
+{
+    // The table associated with the model.
+    protected $Table = "Employees";
+
+    public function FunctionName($value='')
+    {
+
+    }
+    // End function
+    
+}
+// End Class
