4d863bda925e495bfba94ac60765d6eca534615c
Neoceryx <dani-deoz@hotmail.com>
2017-08-27 22:09:52 -0700

    Se agrego el script de Home.js en la vista Home.php

---
+++ GymSys/resources/views/Home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <h4>This is the Home View</h4>
+
+    <!-- Redirect User to the Test Route -->
+    <a href="/Test">Test Link</a>
+
+    <div class="">
+
+      <label for="js_Name">Name:</label>
+      <input id="js_Name" type="text" name="" value="">
+      <br>
+      <button id="js_Send" type="button" name="button">Send</button>
+
+    </div>
+
+
+
+
+  </body>
+
+  <!-- Jquery -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+  <!-- Aditional Js -->
+  <script type="text/javascript" src="/Home.js"></script>
+
+
+</html>
