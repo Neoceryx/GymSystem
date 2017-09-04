8606e3e0574f5065175f17259b7d9e5c3c27259c
Neoceryx <dani-deoz@hotmail.com>
2017-09-04 00:49:36 -0700

    Se creo la llamada ajax para poder subir la imagen de perfil del usuario

--- GymSys/public/NormalEmployee/DashBoard.js
+++ GymSys/public/NormalEmployee/DashBoard.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
 
+  // Get App token
+  let TOKEN=$("meta[name='csrf-token']").attr('content');
+
   $("#js_Mbr").click(function () {
 
     // Open Member register modal
@@ -29,9 +32,64 @@ $(document).ready(function () {
     // Get Member address
     var Addrs=$("#js_MbrAddress").val();
 
-    // Get picture path
-    var PicturePath=$("#js_MbrPotho").val()
-    
+    // Get picture Info
+    var PicturePath=$("#sortpicture").prop('files')[0];
+
+    //
+    var form_data = new FormData();
+
+    form_data.append("file", PicturePath)
+
+    console.log(form_data);
+
+    // Start ajax. User Info
+    $.ajax({
+      type:"POST",
+      url:"/AddMember",
+      data:{MBRNAE:MemName},
+      beforeSend: function (request) {
+
+        return request.setRequestHeader('X-CSRF-Token', TOKEN);
+
+      },
+      success:function (data) {
+        // alert(data);
+        // $(".res").html(data);
+      },
+      error:function () {
+        alert("An Error Ocurred");
+      }
+
+    });
+    // End ajax
+
+    // Start ajax. Only for the image
+    $.ajax({
+      type:"POST",
+      url:"/UploadImg",
+      dataType: 'text',  // what to expect back from the PHP script, if anything
+      cache: false,
+      contentType: false,
+      processData: false,
+      data:form_data,
+      beforeSend: function (request) {
+
+        return request.setRequestHeader('X-CSRF-Token', TOKEN);
+
+      },
+      success:function (data) {
+
+        $(".res").html(data);
+
+      },
+      error:function (e) {
+        alert("An Error Ocurred");
+        $("body").html(e.responseText)
+
+      }
+    });
+    // End ajax
+
   });
   // End click
 
