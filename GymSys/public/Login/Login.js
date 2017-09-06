57d698a4fd5633fc975d10404804f15f1ec62403
Neoceryx <dani-deoz@hotmail.com>
2017-09-05 23:51:27 -0700

    Se crearon las validaciones para redireccionar a los tipos de usuarios a sus Dashboards

--- GymSys/public/Login/Login.js
+++ GymSys/public/Login/Login.js
@@ -3,6 +3,12 @@ $(document).ready(function () {
   // Get App token
   let TOKEN=$("meta[name='csrf-token']").attr('content');
 
+    // Enums. to preent magic numbers in loginstring
+    const EMPLOYEEPART=0;
+    const ROLEPART=1;
+
+
+
   $("#js_BtnLgn").click(function () {
 
     // Get Employee Number
@@ -25,6 +31,47 @@ $(document).ready(function () {
 
         // Display Backend result in the dom element
         $(".result").html(data);
+
+        // Get the login string
+        var LoginString=$(".result").text().split("::");
+debugger
+        // get the empoye parte from login string
+        var EmpPart=LoginString[EMPLOYEEPART];
+
+        // get Employee role from login string
+        var Role=LoginString[ROLEPART];
+
+        // Validate User
+        if (EmpPart == 0) {
+
+          alert("User is not registered");
+
+        }else {
+
+          // Validate if the user is admin
+          if (EmpPart == 1 && Role ==1 ) {
+
+            // Redirect admin to the Dashboard
+            window.location.href = "/Admin";
+
+          }else {
+
+            // Validate if the user is normal
+            if ( EmpPart == 1 && Role == 2 ) {
+
+              // Redirect Noral user to the Dashboard
+              window.location.href = "/Employee";
+
+            }else {
+
+              alert("User or password is wrong, please verify the information");
+
+            }
+
+          }
+
+        }
+        // End Validation
 
       },
       error:function (e) {
