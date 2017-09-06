cb7a3691952c75c79118ffd33be5188ff17e2d57
Neoceryx <dani-deoz@hotmail.com>
2017-09-05 23:04:29 -0700

    Se creo la variable de session que obtiene la ruta de la carpateta del usuario

--- GymSys/public/Home.js
+++ GymSys/public/Home.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
 
-  // Get App token
-  let TOKEN=$("meta[name='csrf-token']").attr('content');
-
   console.log("HELLO from Home.js");
 
   //
@@ -15,42 +12,5 @@ $(document).ready(function () {
     alert("The is: "+Name);
 
   });
-  // End Click
-
-  // Logout User
-  $("#js_LogOut").click(function () {
-
-    // Start ajax
-    $.ajax({
-      type:"POST",
-      url:"/LogOut",
-      data:{},
-      beforeSend: function (request) {
-
-        return request.setRequestHeader('X-CSRF-Token', TOKEN);
-
-      },success:function (data) {
-
-        // Show Backend result in the dom
-        $(".result").html(data);
-
-        // Redirect User to the login view
-        window.location.href = "/";
-
-      },
-      error:function (e) {
-
-        console.log(e.responseText);
-        $(".result").html(e.responseText);
-      }
-
-    });
-    //End ajax
-
-
-  });
-  // End Click
-
-
 
 });
--- GymSys/public/Login/Login.js
+++ GymSys/public/Login/Login.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   $("#js_BtnLgn").click(function () {
 
     // Get Employee Number
-    var EmpNmbr=parseInt($("#js_EmpNumbr").val());
+    var EmpNmbr=$("#js_EmpNumbr").val();
 
     // Get Employee Password
     var EmpPass=$("#js_Pass").val();
@@ -26,46 +26,12 @@ $(document).ready(function () {
         // Display Backend result in the dom element
         $(".result").html(data);
 
-        // Get User
-        var LoginString=$(".result").text().split("::");
-
-        // Get Descision val
-        var UsrExist = parseInt( LoginString[0] );
-
-        // Get Employee Type from the loginstring
-        var EmpType= parseInt( LoginString[1] );
-
-        // Validate if the user is register
-        if (UsrExist == 0 ) {
-
-          // Redirect User to the Home view
-          alert("User is not Registered");
-
-        }else {
-
-          if ( UsrExist==1 && EmpType == 1  ) {
-
-            //  Redirect User to Admin Layout
-            window.location.href = "/Admin";
-
-          }
-          if ( UsrExist == 1 && EmpType == 2 ) {
-
-            // Redirect User to the Normal Employee
-            window.location.href = "/Employee";
-
-          }
-
-        }
-        // End User Validation
-
       },
       error:function (e) {
 
         alert("An error ocurred");
         console.log(e);
         $(".result").html(e.responseText);
-
       }
 
     });
