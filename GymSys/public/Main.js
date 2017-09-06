5bc6d003582f3fe855d82f3f57ba256f1f8ac091
Neoceryx <dani-deoz@hotmail.com>
2017-09-06 00:00:16 -0700

    Se creo el script en js que controlara los efectos de materialize

---
+++ GymSys/public/Main.js
@@ -0,0 +1,18 @@
+/*
+<summary>
+  This Script controlate the Framework effects
+</summary>
+*/
+
+$(document).ready(function () {
+
+  alert("HI");
+
+  // SideNav
+  $(".button-collapse").sideNav();
+
+  // Modals
+  $('.modal').modal();
+
+});
+// End Scope
