15bd0d3edcf86d55121581ac7d31968d64628a81
Neoceryx <dani-deoz@hotmail.com>
2017-09-07 21:45:58 -0700

    Se creo sl script para registar nuevos miembros

--- GymSys/public/NormalEmployee/AddMembers.js
+++ GymSys/public/NormalEmployee/AddMembers.js
@@ -8,6 +8,9 @@ $(document).ready(function () {
     // Start validations rules form
     $("#js_MemForm").validate({
 
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
       rules:{
         jv_MbrName:"required"
 
@@ -30,89 +33,92 @@ $(document).ready(function () {
   // Add New Member
   $("#js_RegMbr").click(function () {
 
-    // Get Member Name
-    var MemName=$("#js_MbrName").val().trim();
-
-    // Get Member FstName
-    var FstName=$("#js_MbrFstName").val().trim();
-
-    // Get Member last Name
-    var LstName=$("#js_MbrLstName").val().trim();
-
-    // Get Member phone number
-    var Phone=$("#js_MbrPhone").val();
-
-    // Get Member email
-    var EmailMbr=$("#js_MbrMail").val();
-
-    // Get Member address
-    var Addrs=$("#js_MbrAddress").val();
-
-    // Get picture Info
-    var PicturePath=$("#js_Photo").prop('files')[0];
-
-    // Get Picture name
-    var PicName=PicturePath.name;
-
+    // Call form rules
+    $("#js_MemForm").valid();
     //
-    var form_data = new FormData();
-
-    // Create file
-    form_data.append("file", PicturePath)
-
-    console.log(form_data);
-
-    // Creates object to the user info
-    var UserInfo={MBRNAE:MemName, FSTNAME:FstName, LSTNAME:LstName, PHONE:Phone, EMAIL:EmailMbr, ADRSS:Addrs, PICNAME:PicName };
-
-    // Start ajax. Send User info
-    $.ajax({
-      type:"POST",
-      url:"/AddMember",
-      data:UserInfo,
-      beforeSend: function (request) {
-
-        return request.setRequestHeader('X-CSRF-Token', TOKEN);
-
-      },
-      success:function (data) {
-        alert(data);
-        // $(".res").html(data);
-      },
-      error:function () {
-        alert("An Error Ocurred");
-        $("body").html(e.responseText)
-      }
-
-    });
-    // End ajax
-
-    // Start ajax. To Upload the image
-    $.ajax({
-      type:"POST",
-      url:"/UploadImg",
-      dataType: 'text',  // what to expect back from the PHP script, if anything
-      cache: false,
-      contentType: false,
-      processData: false,
-      data:form_data,
-      beforeSend: function (request) {
-
-        return request.setRequestHeader('X-CSRF-Token', TOKEN);
-
-      },
-      success:function (data) {
-
-        $(".res").html(data);
-
-      },
-      error:function (e) {
-        alert("An Error Ocurred");
-        $("body").html(e.responseText)
-
-      }
-    });
-    // End ajax
+    // // Get Member Name
+    // var MemName=$("#js_MbrName").val().trim();
+    //
+    // // Get Member FstName
+    // var FstName=$("#js_MbrFstName").val().trim();
+    //
+    // // Get Member last Name
+    // var LstName=$("#js_MbrLstName").val().trim();
+    //
+    // // Get Member phone number
+    // var Phone=$("#js_MbrPhone").val();
+    //
+    // // Get Member email
+    // var EmailMbr=$("#js_MbrMail").val();
+    //
+    // // Get Member address
+    // var Addrs=$("#js_MbrAddress").val();
+    //
+    // // Get picture Info
+    // var PicturePath=$("#js_Photo").prop('files')[0];
+    //
+    // // Get Picture name
+    // var PicName=PicturePath.name;
+    //
+    // //
+    // var form_data = new FormData();
+    //
+    // // Create file
+    // form_data.append("file", PicturePath)
+    //
+    // console.log(form_data);
+    //
+    // // Creates object to the user info
+    // var UserInfo={MBRNAE:MemName, FSTNAME:FstName, LSTNAME:LstName, PHONE:Phone, EMAIL:EmailMbr, ADRSS:Addrs, PICNAME:PicName };
+    //
+    // // Start ajax. Send User info
+    // $.ajax({
+    //   type:"POST",
+    //   url:"/AddMember",
+    //   data:UserInfo,
+    //   beforeSend: function (request) {
+    //
+    //     return request.setRequestHeader('X-CSRF-Token', TOKEN);
+    //
+    //   },
+    //   success:function (data) {
+    //     alert(data);
+    //     // $(".res").html(data);
+    //   },
+    //   error:function () {
+    //     alert("An Error Ocurred");
+    //     $("body").html(e.responseText)
+    //   }
+    //
+    // });
+    // // End ajax
+    //
+    // // Start ajax. To Upload the image
+    // $.ajax({
+    //   type:"POST",
+    //   url:"/UploadImg",
+    //   dataType: 'text',  // what to expect back from the PHP script, if anything
+    //   cache: false,
+    //   contentType: false,
+    //   processData: false,
+    //   data:form_data,
+    //   beforeSend: function (request) {
+    //
+    //     return request.setRequestHeader('X-CSRF-Token', TOKEN);
+    //
+    //   },
+    //   success:function (data) {
+    //
+    //     $(".res").html(data);
+    //
+    //   },
+    //   error:function (e) {
+    //     alert("An Error Ocurred");
+    //     $("body").html(e.responseText)
+    //
+    //   }
+    // });
+    // // End ajax
 
   });
   // End click
