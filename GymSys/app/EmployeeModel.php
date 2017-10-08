14a1687b55997e9297b4ccc6b2acb5606ba532ea
Neoceryx <dani-deoz@hotmail.com>
2017-10-08 12:30:27 -0700

    Se creo el metodo en el model de empleados que obtiene los roles de los empleados

---
+++ GymSys/app/EmployeeModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace GymSys;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+// Allows use DataBases
+use DB;
+
+class EmployeeModel extends Model
+{
+
+  // The table associated with the model.
+  protected $Table = "Members";
+
+  public static function GetEmployeeRoles()
+  {
+
+    // Build the query.
+    $EmpRoles=DB::table("EmpRoles")->get();
+
+    // Iterate over the DataBase Table
+    foreach ( $EmpRoles as $_EmpRole ) {
+
+      // create Objet from the Emproles table. To acces their fiels
+      echo "
+      <option value=".$_EmpRole->Id.">".$_EmpRole->Description."</option>
+      ";
+
+    }
+
+  }
+  // End function
+
+
+}
